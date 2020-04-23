f48ac8424cfc6b3892fd85626e7347e3a446e7f7
unknown <divya.zestard@gmail.com>
2020-04-23 18:27:37 +0530

    Make footer dynamic

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,62 @@
 import PropTypes from "prop-types"
 import React from "react"
-
+import { useStaticQuery, graphql, Link } from "gatsby";
+import { removePre } from './../util/common';
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allWordpressMenusMenusItems(filter: {wordpress_id: {eq: 7}}) {
+      edges {
+        node {
+          name
+          items {
+            title
+            url
+          }
+        }
+      }
+    }
+    allWordpressAcfOptions {
+      edges {
+        node {
+          options {
+            light_site_logo {
+              source_url
+            }
+            contact_email
+            phone_number
+            star_image {
+              source_url
+            }
+            star_content
+            menu_title_with_links {
+              menu_title
+              menu_links {
+                link_title
+                link
+              }
+            }
+            social_media {
+              social_media_icon
+              social_media_link
+              social_media_name
+            }
+            copyrigth
+            external_links {
+              external_image {
+                source_url
+              }
+              external_link
+            }
+          }
+        }
+      }
+    }
+  }
+`)
+const foomenu = data.allWordpressMenusMenusItems.edges[0].node.items;
+const acf = data.allWordpressAcfOptions.edges[0].node.options;
+console.log(foomenu);
   return(
     <footer className="site-footer">
     <div className="main-footer">
@@ -11,7 +66,9 @@ const Footer = () => {
             <div className="footer-col-left">
               <div className="f-left-top">
                 <div className="footer-logo">
-                  {/* <img src="assets/images/logo-white.png" /> */}
+                  {acf.light_site_logo !== null &&
+                    <img src={acf.light_site_logo.source_url} altr="Light footer logo"/>
+                  }                  
                 </div>
                 <div className="contact-info">
                   <ul className="m-0 p-0">
@@ -19,37 +76,32 @@ const Footer = () => {
                       <div className="icon">
                         <i className="fa fa-envelope-o"></i>
                       </div>
-                      <a href="mailto:info@zestard.com">info@zestard.com</a>
+                      <a href={'mailto:'+acf.contact_email}>{acf.contact_email}</a>
                     </li>
                     <li className="d-flex align-items-center">
                       <div className="icon">
                         <i className="fa fa-phone"></i>
                       </div>
-                      <a href="mailto:info@zestard.com">info@zestard.com</a>									
+                      <a href={'tel:'+acf.phone_number}>{acf.phone_number}</a>									
                     </li>
                   </ul>
                 </div>
                 <div className="company-ratings">
                   <div className="figure">
-                    {/* <img src="assets/images/rating.png"> */}
+                    {acf.star_image !== null &&
+                      <img src={acf.star_image.source_url} />
+                    }                    
                   </div>
-                  <p className="m-0">4.6 out of 5.0 for Zestard Technologies by 39 clients reviews.</p>
+                  <p className="m-0">{acf.star_content}</p>
                 </div>
               </div>
               <div className="socials">
                 <ul className="p-0 m-0">
-                  <li>  
-                    <a href="javascript:;"><i className="fa fa-facebook"></i></a>
-                  </li>
-                  <li>
-                    <a href="javascript:;"><i className="fa fa-linkedin"></i></a>
-                  </li>
-                  <li>
-                    <a href="javascript:;"><i className="fa fa-twitter"></i></a>
-                  </li>
-                  <li>
-                    <a href="javascript:;"><i className="fa fa-youtube"></i></a>
-                  </li>
+                  {acf.social_media.map((node,index) => (
+                    <li key={index}>  
+                      <a href={node.social_media_link}><i className={node.social_media_icon}></i></a>
+                    </li>
+                  ))}
                 </ul>
               </div>
             </div>
@@ -58,60 +110,34 @@ const Footer = () => {
             <div className="footer-col-right">
               <div className="f-right-top">
                 <div className="row">
-                  <div className="col-sm-4 footer-col">
+                  {acf.menu_title_with_links.map((node,index) => (
+                    <div className="col-sm-4 footer-col" key={index}>
                     <div className="footer-col-inner">
-                      <h3 className="s-title">Make</h3>
+                      <h3 className="s-title">{node.menu_title}</h3>
                       <ul className="m-0 p-0 s-list">
-                        <li><a href="javascript:;">Ecommerce Development</a></li>
-                        <li><a href="javascript:;">Wordpress Development</a></li>
-                        <li><a href="javascript:;">React JS</a></li>
-                        <li><a href="javascript:;">Node JS</a></li>
+                        {node.menu_links.map((node,index) => (
+                          <li key={index}><a href={node.link}>{node.link_title}</a></li>
+                        ))}
                       </ul>
                     </div>
                   </div>
-                  <div className="col-sm-4 footer-col">
-                    <div className="footer-col-inner">
-                      <h3 className="s-title">Market</h3>
-                      <ul className="m-0 p-0 s-list">
-                        <li><a href="javascript:;">SEO Services</a></li>
-                        <li><a href="javascript:;">Link Building</a></li>
-                        <li><a href="javascript:;">Social Media Marketing</a></li>
-                        <li><a href="javascript:;">PPC / Adwords</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="col-sm-4 footer-col">
-                    <div className="footer-col-inner">
-                      <h3 className="s-title">Maintain</h3>
-                      <ul className="m-0 p-0 s-list">
-                        <li><a href="javascript:;">Magento Maintenance</a></li>
-                        <li><a href="javascript:;">WordPress Maintenance</a></li>
-                        <li><a href="javascript:;">Shopify Maintenance</a></li>
-                        <li><a href="javascript:;">Hire Dedicated Developers</a></li>
-                      </ul>
-                    </div>
-                  </div>
+                  ))}
                 </div>
               </div>
               <div className="f-right-bottom"> 
                 <ul className="f-menu m-0 p-0 d-flex">
-                  <li><a href="javascript:;">About</a></li>
-                  <li><a href="javascript:;">Blog</a></li>
-                  <li><a href="javascript:;">Case Studies</a></li>
-                  <li><a href="javascript:;">Careers</a></li>
-                  <li><a href="javascript:;">Contact</a></li>
-                  <li><a href="javascript:;">Testimonials</a></li>
-                  <li><a href="javascript:;">Partnership</a></li>
+                  {foomenu.map((node,index) => (
+                    <li key={index}><Link to={`/${removePre(node.url)}`}>{node.title}</Link></li>
+                  ))}
                 </ul>
               </div>
               <div className="footer-creadientials">
                 <ul className="p-0 m-0 d-flex justify-content-center">
-                  <li>
-                    {/* <img src="assets/images/m-extension.png" alt=""> */}
+                {acf.external_links.map((node,index) => (
+                  <li key={index}>
+                    <img src={node.external_image.source_url} alt=""/>
                   </li>
-                  <li>
-                    {/* <img src="assets/images/Shopify-app.png" alt=""> */}
-                  </li>
+                  ))}                  
                 </ul>
               </div>
             </div>
@@ -124,14 +150,14 @@ const Footer = () => {
         <div className="row">
           <div className="col-md-8 col-lg-7">
             <div className="copyright-text">
-              <p className="m-0">© 2010 - 2020 Zestard Technologies Pvt Ltd. All rights reserved.</p>
+              <p className="m-0">{acf.copyrigth}</p>
             </div>
           </div>
           <div className="col-md-4 col-lg-5">
             <div className="bottom-link">
               <ul className="m-0 p-0 d-flex justify-content-end">
-                <li><a href="javascript:;">Privacy Policy</a></li>
-                <li><a href="javascript:;">Terms</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms</a></li>
               </ul>
             </div>
           </div>
