c540bcb9412bf48b5e4a135ad0d74efa2751b81e
nisha-zestard <nisha.zetard@gmail.com>
2020-05-15 12:49:56 +0530

    Ecommerce Development Page design

--- src/pages/make/e-commerce-development.js
+++ src/pages/make/e-commerce-development.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import Layout from "./../../components/layout"
 import SEO from "./../../components/seo"
-import AboutProject from './../../components/aboutproject'
+import AboutProject from './../../components/aboutproject' 
 
 class EcommerceDevelopment extends Component {
 
@@ -9,11 +9,212 @@ class EcommerceDevelopment extends Component {
 		return(
 			<Layout>
 				<SEO title="E-commerce Development"/>
-				
-				<section>
-					<AboutProject />				
-				</section>
-	  </Layout>
+				<div id="page" className="ecommerce-development">
+					<section>
+						<div className="sub-services-breadcums">
+							<div className="container">
+								<div className="breadcums-inner">
+									<div className="page-title">
+										<h1>E-commerce Development</h1>
+									</div>
+									<div className="breadcums-wrap">
+										<ul className="d-flex justify-content-center m-0 p-0">
+											<li>
+												<a href="javascript:;">Home</a>
+											</li>
+											<li>
+												<a href="javascript:;">Services</a>
+											</li>
+											<li>
+												<a href="javascript:;">E-commerce Development</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section>
+						<div className="sub-service-baner">
+							<div className="container">
+								<div className="row">
+									<div className="col-md-6 baner-image-wrap">
+										<div className="image-wrap">
+										
+										</div>
+									</div>
+									<div className="col-md-6 baner-content-wrap">
+										<div className="content-wrap">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan nisi arcu, sit amet mattis sem lacinia non. Fusce lobortis, lectus eu mollis lacinia, diam metus eleifend ipsum, 
+											quis sodales nunc metus ac ante. Proin gravida ex libero, quis porttitor est eleifend eget. Donec dictum tortor vel sem interdum dictum at ac lorem. </p>
+											<p>Cras eu sem placerat, gravida ante sed, tempor nisl. Morbi ac metus diam. Morbi condimentum elementum mi.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section>
+						<div className="ecommerce-sercices-wrap">
+							<div className="container">
+								<h2 className="section-title text-center">Our Ecommerce Services</h2>
+								<div className="services-list">
+									<div className="row">
+										<div className="col-md-6 col-lg-4">
+											<div className="service-box">
+												<div className="ss-title">
+													<h2>Ecommerce<br/> Website Design</h2>
+												</div>
+												<div className="ss-content">
+												<p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis tellus ac nisi suscipit.sunt in culpa qui officia 
+													deserunt mollit anim id est laborum.</p>
+												</div>
+											</div>
+										</div>
+										<div className="col-md-6 col-lg-4">
+											<div className="service-box">
+												<div className="ss-title">
+													<h2>Ecommerce<br/> Website Design</h2>
+												</div>
+												<div className="ss-content">
+												<p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis tellus ac nisi suscipit.sunt in culpa qui officia 
+													deserunt mollit anim id est laborum.</p>
+												</div>
+											</div>
+										</div>
+										<div className="col-md-6 col-lg-4">
+											<div className="service-box">
+												<div className="ss-title">
+													<h2>Ecommerce<br/> Website Design</h2>
+												</div>
+												<div className="ss-content">
+												<p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis tellus ac nisi suscipit.sunt in culpa qui officia 
+													deserunt mollit anim id est laborum.</p>
+												</div>
+											</div>
+										</div>
+										<div className="col-md-6 col-lg-4">
+											<div className="service-box">
+												<div className="ss-title">
+													<h2>Ecommerce<br/> Website Design</h2>
+												</div>
+												<div className="ss-content">
+												<p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis tellus ac nisi suscipit.sunt in culpa qui officia 
+													deserunt mollit anim id est laborum.</p>
+												</div>
+											</div>
+										</div>
+										<div className="col-md-6 col-lg-4">
+											<div className="service-box">
+												<div className="ss-title">
+													<h2>Ecommerce<br/> Website Design</h2>
+												</div>
+												<div className="ss-content">
+												<p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis tellus ac nisi suscipit.sunt in culpa qui officia 
+													deserunt mollit anim id est laborum.</p>
+												</div>
+											</div>
+										</div>
+										<div className="col-md-6 col-lg-4">
+											<div className="service-box">
+												<div className="ss-title">
+													<h2>Ecommerce<br/> Website Design</h2>
+												</div>
+												<div className="ss-content">
+												<p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis tellus ac nisi suscipit.sunt in culpa qui officia 
+													deserunt mollit anim id est laborum.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section>
+						<div className="platform-section">
+							<div className="container">
+								<h2 className="section-title text-center">Platforms We Work On</h2>
+								<div className="platform-wrap odd">
+									<div className="row">
+										<div className="col-md-7 platform-image-wrap">
+											<div className="image text-center">
+
+											</div>
+										</div>
+										<div className="col-md-5 platform-content-wrap">
+											<div className="content-inner">
+												<div className="p-title d-flex align-items-center">
+													<img  />
+													<h4>Shopify</h4>
+												</div>
+												<div className="p-desc">
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec dui vel nisl rutrum malesuada id sit amet arcu. Cras ac arcu facilisis, consequat lectus malesuada, placerat massa. Vivamus dapibus orci et ligula efficitur semper. 
+													Sed pulvinar justo eu blandit vestibulum. Nulla quis lorem quis justo vestibulum aliquam in et.</p>
+												</div>
+												<div className="know-more-btn">
+													<a href="javascript:;">Know More</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="platform-wrap even">
+									<div className="row">
+										<div className="col-md-5 platform-content-wrap">
+											<div className="content-inner">
+												<div className="p-title d-flex align-items-center">
+													<img  />
+													<h4>Shopify</h4>
+												</div>
+												<div className="p-desc">
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec dui vel nisl rutrum malesuada id sit amet arcu. Cras ac arcu facilisis, consequat lectus malesuada, placerat massa. Vivamus dapibus orci et ligula efficitur semper. 
+													Sed pulvinar justo eu blandit vestibulum. Nulla quis lorem quis justo vestibulum aliquam in et.</p>
+												</div>
+												<div className="know-more-btn">
+													<a href="javascript:;">Know More</a>
+												</div>
+											</div>
+										</div>
+										<div className="col-md-7 platform-image-wrap">
+											<div className="image text-center">
+
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="platform-wrap odd">
+									<div className="row">
+										<div className="col-md-7 platform-image-wrap">
+											<div className="image text-center">
+
+											</div>
+										</div>
+										<div className="col-md-5 platform-content-wrap">
+											<div className="content-inner">
+												<div className="p-title d-flex align-items-center">
+													<img  />
+													<h4>Shopify</h4>
+												</div>
+												<div className="p-desc">
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec dui vel nisl rutrum malesuada id sit amet arcu. Cras ac arcu facilisis, consequat lectus malesuada, placerat massa. Vivamus dapibus orci et ligula efficitur semper. 
+													Sed pulvinar justo eu blandit vestibulum. Nulla quis lorem quis justo vestibulum aliquam in et.</p>
+												</div>
+												<div className="know-more-btn">
+													<a href="javascript:;">Know More</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section>
+						<AboutProject />				
+					</section>
+				</div>
+	  		</Layout>
 		)
 	}
 
