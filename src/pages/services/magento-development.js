1c6dbd347a71dd6c541d94c48c3705cd4c06d6e6
unknown <divya.zestard@gmail.com>
2020-07-28 19:33:24 +0530

    Created technology pages

--- src/pages/services/magento-development.js
+++ src/pages/services/magento-development.js
@@ -1,37 +1,35 @@
 import React, { Component } from "react"
 import { graphql, Link } from "gatsby"
-import Layout from "./../../components/layout"
+import Layout from "../../components/layout"
 import Header from "../../components/header";
-import SEO from "./../../components/seo"
+import SEO from "../../components/seo"
 import { removePre } from './../../util/common'
-import AboutProject from './../../components/aboutproject' 
+import AboutProject from '../../components/aboutproject'
 
-class EcommerceDevelopment extends Component {
-	render() {	
+class ReactjsDevelopment extends Component {
+	render() {		
 		const data = this.props.data  
-		const acf = data.wordpressPage.acf.gen_content_modules_page
-		const tellus = data.wordpressPage.acf
-		const banner = acf[0].iwc_layout_details[0]
-		const services = acf[1].cs_cards_details
-		const platform = acf[2].iwc_layout_details	
-		//console.log(data.wordpressPage.acf);	
+		const acf = data.allWordpressPage.edges[0].node.acf
+		const pagedata = acf.gen_content_modules_page
+		// const platform = pagedata[2].iwc_layout_details
+		console.log(acf);
 		return(
 			<Layout>
-				<SEO title="E-commerce Development"/>
+				<SEO title="Website Development"/>
 				<Header headernavclass="lightheader" />
-				<div id="page" className="ecommerce-development">
+				<div id="page" className="website-development">
 					<section>
 						<div className="sub-services-breadcums">
 							<div className="container">
 								<div className="breadcums-inner">
 									<div className="page-title">
-										<h1>E-commerce Development</h1>
+										<h1>{pagedata[0].iwc_layout_details[0].iwc_title}</h1>
 									</div>
 									<div className="breadcums-wrap">
 										<ul className="d-flex justify-content-center m-0 p-0">
 											<li><Link to="#">Home</Link></li>
 											<li><Link to="#">Services</Link></li>
-											<li><Link to="#">E-commerce Development</Link></li>
+											<li><Link to="#">{pagedata[0].iwc_layout_details[0].iwc_title}</Link></li>
 										</ul>
 									</div>
 								</div>
@@ -44,25 +42,25 @@ class EcommerceDevelopment extends Component {
 								<div className="row">
 									<div className="col-md-6 baner-image-wrap">
 										<div className="image-wrap">
-											{banner.iwc_image !== null &&
-												<img src={banner.iwc_image.source_url} alt="Service banner"/>
+											{acf.header_mascot !== null &&
+												<img src={pagedata[0].iwc_layout_details[0].iwc_image.source_url} alt="" />
 											}											
 										</div>
 									</div>
 									<div className="col-md-6 baner-content-wrap">
-										<div className="content-wrap" dangerouslySetInnerHTML={{__html: banner.iwc_sub_desc}} />
+										<div className="content-wrap" dangerouslySetInnerHTML={{__html: pagedata[0].iwc_layout_details[0].iwc_sub_desc}} />											
 									</div>
 								</div>
 							</div>
 						</div>
 					</section>
-					{/* <section>
+					<section>
 						<div className="ecommerce-sercices-wrap">
 							<div className="container">
-								<h2 className="section-title text-center">Our Ecommerce Services</h2>
+								<h2 className="section-title text-center">{pagedata[1].cs_section_title}</h2>
 								<div className="services-list">
 									<div className="row">
-										{services.map((node, index) => (
+									{pagedata[1].cs_cards_details.map((node,index) => (
 											<div className="col-md-6 col-lg-4" key={index}>
 												<div className="service-box">
 													<div className="ss-title">
@@ -76,8 +74,8 @@ class EcommerceDevelopment extends Component {
 								</div>
 							</div>
 						</div>
-					</section> */}
-					<section>
+					</section>
+					{/* <section>
 						<div className="platform-section">
 							<div className="container">
 								<h2 className="section-title text-center">Platforms We Work On</h2>
@@ -121,61 +119,80 @@ class EcommerceDevelopment extends Component {
 								))}
 							</div>
 						</div>
-					</section>
-					<AboutProject 
-					apsiwtch={tellus.use_common_contact_section} 
-					apimage={tellus.tuabp_image} 
-					aptitle={tellus.tuabp_title} 
-					apcontent={tellus.tuabp_content} 
-					apbuttontext={tellus.tuabp_button_text} 
-					apbuttonlink={tellus.tuabp_button_link} />
-					{/* <AboutProject apsiwtch={tellus.use_common_contact_section} /> */}
+					</section> */}
 				</div>
-	  		</Layout>
+				<AboutProject 
+					apsiwtch={acf.use_common_contact_section} 
+					apimage={acf.tuabp_image} 
+					aptitle={acf.tuabp_title} 
+					apcontent={acf.tuabp_content} 
+					apbuttontext={acf.tuabp_button_text} 
+					apbuttonlink={acf.tuabp_button_link} />
+	  </Layout>
 		)
 	}
 
 }
-
-export default EcommerceDevelopment
+export default ReactjsDevelopment
 
 export const query = graphql`
 {
-	wordpressPage(wordpress_id: {eq: 1491}) {
-		acf {
-			use_common_contact_section
-			  tuabp_title
-			  tuabp_image {
-				source_url
-			  }
-			  tuabp_content
-			  tuabp_button_text
-			  tuabp_button_link
-		  gen_content_modules_page {
-			... on WordPressAcf_gen_image_with_content {
-			  id
-			  iwc_layout_details {
-				iwc_title
-				iwc_image {
+	allWordpressPage(filter: {wordpress_id: {eq: 7449}}) {
+		edges {
+			node {
+			  acf {
+				header_sub_text
+				header_section_title
+				header_mascot {
 				  source_url
 				}
-				iwc_icon {
-					source_url
+				header_page_title
+				home_mascot_class
+				use_common_contact_section
+				tuabp_title
+				tuabp_image {
+				  source_url
+				}
+				tuabp_content
+				tuabp_button_text
+				tuabp_button_link
+				gen_content_modules_page {
+				  ... on WordPressAcf_gen_image_with_content {
+					id
+					iwc_layout_details {
+					  iwc_image {
+						source_url
+					  }
+					  iwc_sub_desc
+					  iwc_icon {
+						source_url
+					  }
+					  iwc_section_class
+					  iwc_title
+					}
 				  }
-				iwc_sub_desc
-				iwc_section_class
-				iwc_button_text
-                iwc_button_link
-			  }
-			}
-			... on WordPressAcf_gen_cards_section {
-			  id
-			  cs_cards_details {
-				cs_title
-				cs_content
+				  ... on WordPressAcf_gen_cards_section {
+					id
+					cs_section_title
+					  cs_section_class
+					cs_cards_details {
+					  cs_icon {
+						source_url
+					  }
+					  cs_title
+					  cs_content
+					  cs_learn_more_link
+					}
+				  }
+				  ... on WordPressAcf_gen_case_study_section {
+					css_title
+					css_content
+					css_select_case_studies
+					css_section_class
+				  }
+				}
 			  }
 			}
 		  }
-		}
-	  }  
+	  }
 }`
