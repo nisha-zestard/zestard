f818f524befe2882f07aa9693affaf3bd3c59683
nisha-zestard <nisha.zetard@gmail.com>
2020-10-27 16:19:02 +0530

    About Us Page Design

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react"
-import Layout from "../../components/layout"
-import SEO from "../../components/seo";
+import Layout from "./../components/layout"
+import SEO from "../components/seo";
 
 
 class Contact extends Component {
-  
-    render() {
 
-      return (
-        <Layout>
-            <SEO title="Contact"/>
-            <div>Contact us page</div>
-        </Layout>
-        )
-  }
+render() {
+
+return (
+<Layout>
+<SEO title="Contact"/>
+<div>Contact us page</div>
+</Layout>
+)
+}
 
 }
 export default Contact
